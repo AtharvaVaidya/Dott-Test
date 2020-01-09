5e82227c2f085f5a21719c934642ad95cd8d0c04
Atharva Vaidya <atharva.vaidya@icloud.com>
2020-01-09 13:34:18 +0530

    Added API Client abstraction

---
+++ Dott-Test/NetworkError.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkError.swift
+//  Dott-Test
+//
+//  Created by Atharva Vaidya on 1/9/20.
+//  Copyright © 2020 Atharva Vaidya. All rights reserved.
+//
+
+import Foundation
+
+/// Possible networking errors
+enum NetworkError: Error {
+    case badResponse
+    case badStatusCode
+    case failedToParseJSONData
+    case failedToParseImageData(_: Data)
+}
